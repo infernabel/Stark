2d026b6e9cd60416dbcedd7a900ae98724f66d39
infernabel <161008355+infernabel@users.noreply.github.com>
2025-09-04 22:30:38 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { componentTagger } from "lovable-tagger";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
-    host: "https://stark-8q2g.onrender.com",
+    host: "::",
     port: 8080,
   },
   plugins: [
