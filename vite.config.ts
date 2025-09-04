6ac53dd23a398ba3ec1b40f0d2f682f0de24d11d
infernabel <161008355+infernabel@users.noreply.github.com>
2025-09-04 21:34:52 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,14 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+import { componentTagger } from "lovable-tagger";
+
+// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/',
   server: {
-    host: true, // allows all hosts
+    host: "::",
     port: 8080,
-    allowedHosts: ['stark-8q2g.onrender.com'], // your Render host
+    allowedHosts: ['all'], // allows all hosts/domains
   },
   plugins: [
     react(),
